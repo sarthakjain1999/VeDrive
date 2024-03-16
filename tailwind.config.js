7abe97f9ef5ea6b5d624881c580d8613c1470dd8
Sarthak Jain <sarthakjain1999@outlook.com>
2024-03-16 18:44:08 +0000

    Install tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'media', /* or 'media' or 'class' */
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
